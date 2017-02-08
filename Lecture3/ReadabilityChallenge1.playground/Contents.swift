8559a29fc639e4ee99d21b43676db9cbcc6c5565
Van Simmons <rvsrvs@gmail.com>
2017-02-08 10:37:54 -0500

    One more readability improvement

--- Lecture3/ReadabilityChallenge1.playground/Contents.swift
+++ Lecture3/ReadabilityChallenge1.playground/Contents.swift
@@ -1,19 +1,15 @@
 
-func isLeap(_ year:Int) ->  Bool {
-    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
-}
+func isLeap(_ year:Int) ->  Bool { return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 }
 
 let nonLeapMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 let leapMonths    = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 func julianDate(year: Int, month: Int, day: Int) -> Int {
     let months = isLeap(year) ? leapMonths : nonLeapMonths
-    let yearDays = (1900 ..< year).reduce(0) { (days, year) in return days + (isLeap(year) ? 366 : 365) }
+    let yearDays = (1900 ..< year).reduce(0) {  return $0 + (isLeap($1) ? 366 : 365) }
     let monthDays = (0 ..< month - 1).reduce(0) { return $0 + months[$1] }
     return yearDays + monthDays + day
 }
-
-isLeap(1900)
 
 julianDate(year: 1960, month:  9, day: 28)
 julianDate(year: 1900, month:  1, day:  1)
